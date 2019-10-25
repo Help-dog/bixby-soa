49a817b3265e34a22c8cc7fb4d60ef068781655e
EunJu <qjemfrkwl23@gmail.com>
2019-10-25 17:39:59 +0900

    change fromHospitalName.js

--- idear.soa/code/hospital/fromHospitalName.js
+++ idear.soa/code/hospital/fromHospitalName.js
@@ -1,7 +1,8 @@
-module.exports.function = function fromHospitalName (hospitalName, self, point) {
+module.exports.function = function fromHospitalName (hospitalName, self, point, recommendation) {
   var http = require('http')
   const console = require('console');
 
+  var rec = recommendation;
   var response = http.getUrl('https://dapi.kakao.com/v2/local/search/keyword.json?y='+point.point.latitude+'&x='+point.point.longitude+'&radius=2000', {
   format: 'json',
   query: {
@@ -12,20 +13,24 @@ module.exports.function = function fromHospitalName (hospitalName, self, point) 
   }
 })
 
-  if(response.documents[0] === undefined || response.documents[0].category_name.indexOf("병원") == -1) {
-    return null;
-  } else {
-    return {
-      phone: response.documents[0].phone,
-      placeName: response.documents[0].place_name,
-      url: response.documents[0].place_url,
-      roadAddressName: response.documents[0].road_address_name,
+  hospitalName = hospitalName.replace(/(\s*)/g, "");
+
+for(var i=0; i<response.documents.length; i++) {
+ if(response.documents[i].place_name.indexOf(hospitalName)==-1 || response.documents[i] === undefined || response.documents[i].category_name.indexOf("병원") == -1){
+   continue;
+ } else {
+   return{
+     phone: response.documents[i].phone,
+      placeName: response.documents[i].place_name,
+      url: response.documents[i].place_url,
+      roadAddressName: response.documents[i].road_address_name,
       point: {
         point: {
-          latitude: response.documents[0].y,
-          longitude: response.documents[0].x
+          latitude: response.documents[i].y,
+          longitude: response.documents[i].x
         }
       }
-    } 
-  }
+   }
+ }
+}
 }
