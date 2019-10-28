125124408d2ae4844f4ef46b24442435ae8eba18
최훈석 <gnstjr5656@naver.com>
2019-10-28 10:32:42 +0900

    S1P3151001-76 | 코드정리 완료

--- idear.soa/code/vaccine/searchVaccine.js
+++ idear.soa/code/vaccine/searchVaccine.js
@@ -1,22 +1,21 @@
 module.exports.function = function searchVaccine (vaccinationName) {
-const console = require('console');
-console.log("name"+vaccinationName);
+  const console = require('console');
+  console.log("name"+vaccinationName);
 
-const vaccineData = require("../data/vaccine/vaccineData.js");
+  const vaccineData = require("../data/vaccine/vaccineData.js");
 
-var imgUrl = "img/vaccineBaby2.jpg";
-var result = {};
+  var imgUrl = "img/vaccineBaby2.jpg";
+  var result = {};
 
-console.log(vaccineData[0])
-for(let i=0; i<vaccineData.length; i++){
-  if(vaccineData[i].name.indexOf(vaccinationName.toString())>=0){
-    result = {vaccinationName: vaccinationName, 
-              vaccinationDesc: vaccineData[i].info,
-              imgUrl: imgUrl
-              };
-    break;
+  console.log(vaccineData[0])
+  for(let i=0; i<vaccineData.length; i++){
+    if(vaccineData[i].name.indexOf(vaccinationName.toString())>=0){
+      result = {vaccinationName: vaccinationName, 
+                vaccinationDesc: vaccineData[i].info,
+                imgUrl: imgUrl
+      };
+      break;
+    }
   }
-}
   return result;
-  
 }
--- idear.soa/scenario/vaccinCheck.md
+++ idear.soa/scenario/vaccinCheck.md
@@ -1,16 +1,16 @@
-예방접종 체크
-[목적]
-0~35개월 몇세 입력받으면 그 때 받아야할 예방 접종 목록 표시
-[사용자가 입력할 말]
-2개월 예방접종 알려줘
-4세 예방접종 알려줘
+#예방접종 체크
+#[목적]
+#0~35개월 몇세 입력받으면 그 때 받아야할 예방 접종 목록 표시
+#[사용자가 입력할 말]
+#2개월 예방접종 알려줘
+#4세 예방접종 알려줘
 
-[발화함수 Action]
+#[발화함수 Action]
 #VaccinCheck
 
-[발화변수 Input Concept]
-X개월 - WhatMonth
-예방접종 - Vaccin
+#[발화변수 Input Concept]
+#X개월 - WhatMonth
+#예방접종 - Vaccin
 
-[발화변수 Output Concept]
+#[발화변수 Output Concept]
 #ResultsVaccin
