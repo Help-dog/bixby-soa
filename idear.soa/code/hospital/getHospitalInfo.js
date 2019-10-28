52ca25acc22797425c53b41f182f6086b96586c4
EunJu <qjemfrkwl23@gmail.com>
2019-10-28 09:36:00 +0900

    hospital 관련 코드 cleancode

--- idear.soa/code/hospital/getHospitalInfo.js
+++ idear.soa/code/hospital/getHospitalInfo.js
@@ -6,8 +6,7 @@ function getDistance(lat1,lon1,lat2,lon2) {
   const a = 
     Math.sin(dLat/2) * Math.sin(dLat/2) +
     Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ; 
+    Math.sin(dLon/2) * Math.sin(dLon/2); 
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
   const d = R * c; // Distance in km
   return d;
@@ -29,7 +28,6 @@ module.exports.function = function getHospitalInfo (near, point, self, recommend
 })
 
   let datas = [];
-
   for(let i=0; i<response.documents.length; i++) {
     var data = {
       name: response.documents[i].place_name,
