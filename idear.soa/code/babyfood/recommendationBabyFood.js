bbad5f3842b4343e3a827dd15d88929d0d43140e
최훈석 <gnstjr5656@naver.com>
2019-10-29 11:57:10 +0900

    S1P3151001-80 | 이유식 코드 정리중

--- idear.soa/code/babyfood/recommendationBabyFood.js
+++ idear.soa/code/babyfood/recommendationBabyFood.js
@@ -9,75 +9,93 @@ module.exports.function = function searchBabyFood(month, ingredients, babyFood, 
 	if (ingredients) {
 		ingredients = ingredients.replace(/(\s*)/g, "");
 	}
-
-	for (var i = 0; i < babyfoodlist.length; i++) {
-		if()
-	}
-	if (4 <= month && month <= 6) {
-		for (var i = 0; i < babyfoodlist.length; i++) {
-			if (ingredients) {
-				if (babyfoodlist[i].foodIngredients.indexOf(ingredients.toString()) != -1 && babyfoodlist[i].category === "4~6개월 이유식")
-					result.push(babyfoodlist[i]);
-			} else {
-				if (babyfoodlist[i].category === "4~6개월 이유식") {
-					result.push(babyfoodlist[i]);
-				}
-			}
-		}
-	}
-	else if (7 <= month && month <= 9) {
-		for (var i = 0; i < babyfoodlist.length; i++) {
-			if (ingredients) {
-				if (babyfoodlist[i].foodIngredients.indexOf(ingredients.toString()) != -1 && babyfoodlist[i].category === "7~9개월 이유식")
-					result.push(babyfoodlist[i]);
-			} else {
-				if (babyfoodlist[i].category === "7~9개월 이유식") {
-					result.push(babyfoodlist[i]);
-				}
-			}
-		}
-	}
-	else if (10 <= month && month <= 12) {
-		for (var i = 0; i < babyfoodlist.length; i++) {
-			if (ingredients) {
-				if (babyfoodlist[i].foodIngredients.indexOf(ingredients.toString()) != -1 && babyfoodlist[i].category === "10~12개월 이유식")
-					result.push(babyfoodlist[i]);
-			} else {
-				if (babyfoodlist[i].category === "10~12개월 이유식") {
-					result.push(babyfoodlist[i]);
-				}
-			}
-		}
-	}
-	else if (13 <= month && month <= 24) {
-		for (var i = 0; i < babyfoodlist.length; i++) {
-			if (ingredients) {
-				if (babyfoodlist[i].foodIngredients.indexOf(ingredients.toString()) != -1
-					&& babyfoodlist[i].category === "13~24개월 이유식" || babyfoodlist[i].category === "유아식")
-					result.push(babyfoodlist[i]);
-			} else {
-				if (babyfoodlist[i].category === "13~24개월 이유식" || babyfoodlist[i].category === "유아식") {
-					result.push(babyfoodlist[i]);
-				}
-			}
-		}
-	}
-	else if (month <= 3 || month >= 25){
+	
+	if (month <= 3 || month >= 25){ //예외처리
 		return null;
 	}
 
-	if (!month) {
+	if(month){ //달이있다
+		for(var i =0; i< babyfoodlist.length; i++){
+			if(ingredients){ // 재료가 있다
+				if (babyfoodlist[i].foodIngredients.indexOf(ingredients.toString()) != -1 && babyfoodlist[i].category.indexOf(month.toString()) != -1)
+					result.push(babyfoodlist[i]);
+			}else{ //재료가 없다
+				if(babyfoodlist[i].category.indexOf(month.toString()) != -1)
+					result.push(babyfoodlist[i]);
+			}
+		}
+	}else{//달이 없다
 		for (var i = 0; i < babyfoodlist.length; i++) {
-			if (ingredients) {
+			if (ingredients) { //재료가있다
 				if (babyfoodlist[i].foodIngredients.indexOf(ingredients.toString()) != -1) {
 					result.push(babyfoodlist[i]);
 				}
-			} else {
+			} else { //재료가없다
 				result.push(babyfoodlist[i]);
 			}
 		}
 	}
 
+	// if (4 <= month && month <= 6) {
+	// 	for (var i = 0; i < babyfoodlist.length; i++) {
+	// 		if (ingredients) {
+	// 			if (babyfoodlist[i].foodIngredients.indexOf(ingredients.toString()) != -1 && babyfoodlist[i].category === "4~6개월 이유식")
+	// 				result.push(babyfoodlist[i]);
+	// 		} else {
+	// 			if (babyfoodlist[i].category === "4~6개월 이유식") {
+	// 				result.push(babyfoodlist[i]);
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// else if (7 <= month && month <= 9) {
+	// 	for (var i = 0; i < babyfoodlist.length; i++) {
+	// 		if (ingredients) {
+	// 			if (babyfoodlist[i].foodIngredients.indexOf(ingredients.toString()) != -1 && babyfoodlist[i].category === "7~9개월 이유식")
+	// 				result.push(babyfoodlist[i]);
+	// 		} else {
+	// 			if (babyfoodlist[i].category === "7~9개월 이유식") {
+	// 				result.push(babyfoodlist[i]);
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// else if (10 <= month && month <= 12) {
+	// 	for (var i = 0; i < babyfoodlist.length; i++) {
+	// 		if (ingredients) {
+	// 			if (babyfoodlist[i].foodIngredients.indexOf(ingredients.toString()) != -1 && babyfoodlist[i].category === "10~12개월 이유식")
+	// 				result.push(babyfoodlist[i]);
+	// 		} else {
+	// 			if (babyfoodlist[i].category === "10~12개월 이유식") {
+	// 				result.push(babyfoodlist[i]);
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// else if (13 <= month && month <= 24) {
+	// 	for (var i = 0; i < babyfoodlist.length; i++) {
+	// 		if (ingredients) {
+	// 			if (babyfoodlist[i].foodIngredients.indexOf(ingredients.toString()) != -1
+	// 				&& babyfoodlist[i].category === "13~24개월 이유식" || babyfoodlist[i].category === "유아식")
+	// 				result.push(babyfoodlist[i]);
+	// 		} else {
+	// 			if (babyfoodlist[i].category === "13~24개월 이유식" || babyfoodlist[i].category === "유아식") {
+	// 				result.push(babyfoodlist[i]);
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// if (!month) {
+	// 	for (var i = 0; i < babyfoodlist.length; i++) {
+	// 		if (ingredients) {
+	// 			if (babyfoodlist[i].foodIngredients.indexOf(ingredients.toString()) != -1) {
+	// 				result.push(babyfoodlist[i]);
+	// 			}
+	// 		} else {
+	// 			result.push(babyfoodlist[i]);
+	// 		}
+	// 	}
+	// }
 	if (result.length > 6) {
 		number = randomNum(result.length);
 		for (var i = 0; i < 6; i++) {
