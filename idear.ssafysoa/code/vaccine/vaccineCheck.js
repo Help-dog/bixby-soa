6692c73cbf2194ff4b7557024bb62a67708a2aa0
최훈석 <gnstjr5656@naver.com>
2019-10-30 12:06:14 +0900

    버그 수정

--- idear.ssafysoa/code/vaccine/vaccineCheck.js
+++ idear.ssafysoa/code/vaccine/vaccineCheck.js
@@ -1,4 +1,4 @@
-module.exports.function = function vaccineCheck (whatMonth,choiseVaccine,recommendation) {
+module.exports.function = function vaccineCheck (whatMonth,choiseVaccine,recommendation,month) {
 
   const console = require('console');
   const nameinfo = require("../data/vaccine/vaccineData.js");
@@ -10,11 +10,18 @@ module.exports.function = function vaccineCheck (whatMonth,choiseVaccine,recomme
   var http = require('http');
   var site = "https://nip.cdc.go.kr/irgd/index.html"
   var imgUrl = "img/vaccineBaby.jpg";
-  if(whatMonth == undefined){
+  if(whatMonth == undefined && month == undefined){
     return  result;
   }
+  if(whatMonth == "ex"){
+    return  result;
+  }
+
+  console.log("데이터입력 확인"+whatMonth)
+  console.log("데이터입력 확인"+month)
+
   for(let i=0; i<datainfo.length; i++){
-    if(datainfo[i].month.indexOf(whatMonth.toString())>=0){
+    if(whatMonth != undefined && datainfo[i].month.indexOf(whatMonth.toString())>=0){
       var S= datainfo[i].info.split(',') 
       var data = [];
       for(var s in S){
@@ -28,6 +35,29 @@ module.exports.function = function vaccineCheck (whatMonth,choiseVaccine,recomme
         }
         data = {
           vaccinDate: whatMonth+"에"+" "+"접종해야 할 백신 정보입니다.",
+          vaccinInfo: S[s],
+          vaccinInfo2: depdata,
+          url: site,
+          imgUrl: imgUrl
+        };
+        result.push(data);
+      }
+      break;
+    }
+    else if(month != undefined && datainfo[i].month.indexOf(month.toString())>=0){
+      var S= datainfo[i].info.split(',') 
+      var data = [];
+      for(var s in S){
+        var vaccinInfo2
+        var dataN = S[s].slice(0,4)
+        for(var d in nameinfo){
+          if((nameinfo[d].name.indexOf(dataN))>=0){
+            depdata = nameinfo[d].info
+            break;
+          }
+        }
+        data = {
+          vaccinDate: month+"개월에"+" "+"접종해야 할 백신 정보입니다.",
           vaccinInfo: S[s],
           vaccinInfo2: depdata,
           url: site,
