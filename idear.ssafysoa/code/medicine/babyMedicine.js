c5e5085681f3dd83877ef727f004ac4bc1ad04c2
최훈석 <gnstjr5656@naver.com>
2019-11-06 10:38:37 +0900

    S1P3151001-108 | 상비약 최종

--- idear.ssafysoa/code/medicine/babyMedicine.js
+++ idear.ssafysoa/code/medicine/babyMedicine.js
@@ -8,15 +8,21 @@ module.exports.function = function babyMedicine (medicine, recommendation) {
   var medicineName
   var medicineUse
   
+  
+  
   for( i in nameinfo){
     let name = nameinfo[i].name
     let use = nameinfo[i].use
+    let num = nameinfo[i].num
     let data = []
     console.log(name +":"+use)
-
+    let url ="img/medicine/"+num+".PNG";
+    console.log(url)
+    // url = "img/vaccineBaby.jpg";
     data = {
       medicineName : name,
-      medicineUse : use
+      medicineUse : use,
+      imgUrl: url
     }
     console.log(data)
     result.push(data);
